5f712492cc852be64c1cd95d25f9c6d78adb47fe
Joshua Vial <joshua@enspiral.com>
2015-09-11 04:07:47 +1200

    basic page

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,10 @@ module ObjetiveMapperPrototype
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.generators do |g|
-      g.helpers false
+      g.helper false
       g.test_framework :rspec
+      g.view_specs false
+      g.template_engine :haml
     end
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
